fc179057e16672b09df0d7c6ebc4e73a4788952f
John Kasun <johnkasun@live.com>
2022-03-24 15:56:25 -0400

    update CMake and add initial source files

---
+++ src/MainComponent.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <juce_gui_extra/juce_gui_extra.h>
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponent : public juce::Component
+{
+public:
+    //==============================================================================
+    MainComponent();
+    ~MainComponent() override;
+
+    //==============================================================================
+    void paint(juce::Graphics& g) override;
+    void resized() override;
+
+private:
+    //==============================================================================
+    // Your private member variables go here...
+
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+};
